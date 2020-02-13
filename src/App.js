d0123458cb27d57af1de6f94bc1d95043b89cb02
sunqp <sunqp@meixing.com>
2020-02-13 14:42:29 +0800

    add callback function

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React, {Component} from 'react';
 import './App.css';
 import {Button, ButtonToolbar,} from 'react-bootstrap';
 
+const { ipcRenderer } = window.require('electron')
+
 class App extends Component {
+
+    componentWillMount() {
+        ipcRenderer.on('start-canbox-complete', (event, arg) => {
+            console.log('start complete.');
+        });
+    }
 
     _showAlert() {
         alert('Hello');
         console.log('hello where.');
     }
 
-    showLog() {
+    showLog = () => {
       console.log("where hello");
+      ipcRenderer.send('start-canbox', 'start command');
     }
-    
+
     render() {
         return (
             <div className="App">
