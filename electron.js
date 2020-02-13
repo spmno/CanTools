87b0c0c23e7bbb17dbb9af9118556cbb06fbb133
sunqp <sunqp@meixing.com>
2020-02-13 14:44:44 +0800

    fixed electron.js file

--- electron.js
+++ electron.js
@@ -1,91 +1,41 @@
-#include <napi.h>
-#include <iostream>
-#include <memory>
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path');
+const url = require('url')
+const pkg = require('../package.json')
+const canbox = require('./Release/canbox')
 
-#include "./cpp/timer.hpp"
 
-using namespace std;
-
-Napi::ThreadSafeFunction tsfn;
-std::thread nativeThread;
-static Timer timer;
-
-Napi::Boolean StartBox(const Napi::CallbackInfo& info) {
-  Napi::Env env = info.Env();
-  cout << "start box from native." << endl;
-  return Napi::Boolean::New(env, true);
-}
-
-Napi::Value SendInfoLoop( const Napi::CallbackInfo& info )
-{
-  Napi::Env env = info.Env();
-
-  if ( info.Length() < 2 )
-  {
-    throw Napi::TypeError::New( env, "Expected two arguments" );
-  }
-  else if ( !info[0].IsFunction() )
-  {
-    throw Napi::TypeError::New( env, "Expected first arg to be function" );
-  }
-  else if ( !info[1].IsNumber() )
-  {
-    throw Napi::TypeError::New( env, "Expected second arg to be number" );
-  }
-
-  int count = info[1].As<Napi::Number>().Int32Value();
-
-  // Create a ThreadSafeFunction
-  tsfn = Napi::ThreadSafeFunction::New(
-      env,
-      info[0].As<Napi::Function>(),  // JavaScript function called asynchronously
-      "Resource Name",         // Name
-      0,                       // Unlimited queue
-      1,                       // Only one thread will use this initially
-      []( Napi::Env ) {        // Finalizer used to clean threads up
-        nativeThread.join();
-      } );
-
-  // Create a native thread
-  nativeThread = std::thread( [count] {
-    auto callback = []( Napi::Env env, Napi::Function jsCallback, int* value ) {
-      // Transform native data into JS data, passing it to the provided 
-      // `jsCallback` -- the TSFN's JavaScript function.
-      jsCallback.Call( {Napi::Number::New( env, *value )} );
-      
-      // We're finished with the data.
-      delete value;
-    };
-
-    for ( int i = 0; i < count; i++ )
-    {
-      // Create new data
-      int* value = new int( clock() );
-
-      // Perform a blocking call
-      napi_status status = tsfn.BlockingCall( value, callback );
-      if ( status != napi_ok )
-      {
-        // Handle error
-        break;
-      }
-
-      std::this_thread::sleep_for( std::chrono::seconds( 1 ) );
+function createWindow () {
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
     }
+  })
 
-    // Release the thread-safe function
-    tsfn.Release();
-  } );
+  // and load the index.html of the app.
+  if (pkg.devMode) 
+    win.loadURL('http://localhost:3000/');
+  else 
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, './index.html'),
+      protocol: 'file',
+      slashes: true
+    }));
 
-  return Napi::Boolean::New(env, true);
+    console.log('app started.');
 }
 
+app.whenReady().then(createWindow)
 
-
-Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  exports.Set("startBox", Napi::Function::New(env, StartBox));
-  exports.Set("sendInfoLoop", Napi::Function::New(env, SendInfoLoop));
-  return exports;
-}
-
-NODE_API_MODULE(canbox, Init)
+ipcMain.on('start-canbox', (event, arg) => {
+  console.log("start canbox message comming.");
+  //canbox.startBox();
+  canbox.sendInfoLoop(function () {
+    console.log("JavaScript callback called with arguments", Array.from(arguments));
+  }, 5);
+  console.log("print startbox");
+  event.reply('start-canbox-complete', "start commplete.");
+});
